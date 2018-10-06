6d6d1b89ca64a8b53bf3b4533865e01aa9e4b4c2
daniel_triana1993 <daniel_triana1993@hotmail.com>
2018-10-05 21:52:13 -0400

    guardando articulos

--- blog/app/controllers/articulos_controller.rb
+++ blog/app/controllers/articulos_controller.rb
@@ -1,2 +1,16 @@
 class ArticulosController < ApplicationController
+  def new
+  end
+  
+ def create
+  @articulo = Articulo.new(articulo_params)
+ 
+  @articulo.save
+  redirect_to @articulo
+end
+ 
+private
+  def articulo_params
+    params.require(:articulo).permit(:titular, :contenido)
+  end
 end
