02373f480587a0f8a28ff7f7bf3f7c1f0dc6edd5
u1201426 <u1201426@unimilitar.edu.co>
2018-10-10 22:10:56 -0400

    aadiendo rutas

--- blog/app/models/articulo.rb
+++ blog/app/models/articulo.rb
@@ -1,4 +1,5 @@
 class Articulo < ActiveRecord::Base
+  has_many :comentarios
   validates :titular , presence: true,
   length: { minimum: 5}
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :articulos
+  resources :articulos do
+    resources :comentarios
+  end
   # You can have the root of your site routed with "root"
    root 'bienvenido#index'
 
